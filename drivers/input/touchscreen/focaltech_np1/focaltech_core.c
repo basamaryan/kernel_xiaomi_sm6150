62d71be7318ad17a35385a40f53e39c9c4b4d4c0
basamaryan <basam.aryan@gmail.com>
2023-06-05 21:00:13 +0000

    input: touchscreen: ft8057: s/fts/fts_K6
    Change-Id: I41b0abeb363ff302fa64517e5f5f743eeff8039a

--- drivers/input/touchscreen/focaltech_np1/focaltech_core.c
+++ drivers/input/touchscreen/focaltech_np1/focaltech_core.c
@@ -2185,7 +2185,7 @@ static const struct spi_device_id fts_ts_id[] = {
     {},
 };
 static const struct of_device_id fts_dt_match[] = {
-    {.compatible = "focaltech,fts_ts", },
+    {.compatible = "focaltech,fts_K6", },
     {},
 };
 MODULE_DEVICE_TABLE(of, fts_dt_match);
